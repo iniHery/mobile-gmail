68063a7873d68fbee204f4df2097bf034e179e0e
inihery <idabagusheryana60@gmail.com>
2021-09-29 10:57:34 +0800

    first commit

---
+++ pages/akun.js
@@ -0,0 +1,78 @@
+import Link from "next/link";
+
+const AkunPage = () => {
+  return (
+    <div className="akun">
+      <div className="header-akun">
+        <Link href="/">
+          <a>
+            <img className="btn-cancel" src="/cancel.png" />
+          </a>
+        </Link>
+        <img className="brand" src="/google.png" />
+      </div>
+      <div className="wrapping-profile">
+        <div className="account">
+          <img src="/profile.png" />
+          <div className="description-akun">
+            <h4>Sumanto</h4>
+            <p>
+              sumanto666@gmail.com <span>99+</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="kelola-akun">
+        <h3>Kelola Akun Google Anda</h3>
+      </div>
+
+      <hr/>
+
+      <div className="wrapping-profile">
+        <div className="account">
+          <img src="/profile.png" />
+          <div className="description-akun">
+            <h4>Sumanto</h4>
+            <p>
+              sumanto666@gmail.com <span>99+</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="wrapping-profile">
+        <div className="account">
+          <img src="/profile.png" />
+          <div className="description-akun">
+            <h4>Sumanto</h4>
+            <p>
+              sumanto666@gmail.com <span>99+</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="wrapping-profile">
+        <div className="account">
+          <img src="/profile.png" />
+          <div className="description-akun">
+            <h4>Sumanto</h4>
+            <p>
+              sumanto666@gmail.com <span>99+</span>
+            </p>
+          </div>
+        </div>
+      </div><div className="wrapping-profile">
+        <div className="account">
+          <img src="/profile.png" />
+          <div className="description-akun">
+            <h4>Sumanto</h4>
+            <p>
+              sumanto666@gmail.com <span>99+</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AkunPage;
